2dd2fcb3fde3e752b514ae4a28378778855bdb66
Pavel Rakov <p.rakov@elfsight.com>
2021-11-29 19:52:12 +0700

    Initialisation

---
+++ index.js
@@ -0,0 +1,22 @@
+function iOS() {
+    return [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ].includes(navigator.platform)
+    // iPad on iOS 13 detection
+    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  }
+
+  const viberButton = document.querySelector('.viber-button');
+
+  console.log(iOS());
+
+  if (iOS()) {
+    viberButton.setAttribute('href', 'viber://add?number=639985607431')
+  } else {
+    viberButton.setAttribute('href', 'viber://chat?number=639985607431')
+  }
